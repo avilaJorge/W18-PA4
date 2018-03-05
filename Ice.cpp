3ee40fa1a173dc5033c9b728668a3da387b4e2af
Jorge Avila <avilajorge314@gmail.com>
2018-03-05 14:21:19 -0800

    Ice.cpp done and tested using autograder

--- Ice.cpp
+++ Ice.cpp
@@ -1,16 +1,43 @@
 // CSE 101 Winter 2018, PA 4
 //
-// Name: TODO put both partners' info if applicable
-// PID: TODO
-// Sources of Help: TODO
+// Name: Jorge Avila & Timothy Walker
+// PID:  A14226029   & A14191564
+// Sources of Help: PA4 Review
 // Due: March 10th, 2018 at 11:59 PM
 
 #ifndef __ICE_CPP__
 #define __ICE_CPP__
 
 #include "Ice.hpp"
+#include <climits>
 
 int ice(TwoD_Array<int> rooms) {
-  return 0;
+  int M = rooms.getNumRows();
+  int N = rooms.getNumCols(); 
+  TwoD_Array<int> paths(M, N); 
+
+  for(int i = 0; i < M; i++) {
+    for(int j = 0; j < N; j++) {
+      int temp = rooms.at(i, j);
+      if(i == 0 && j == 0) {
+        if(temp <= 0) {
+          paths.at(0, 0) = 1;
+        } else {
+          paths.at(0, 0) = rooms.at(0,0) + 1;
+        }
+      }
+      else if(i == M-1 && j == N-1) {
+        paths.at(i,j) = MAX(paths.at(i, j-1) + temp, paths.at(i-1,j) + temp);
+      }
+      else if(i - 1 < 0) {
+        paths.at(i,j) = paths.at(i,j-1) + temp;
+      } else if(j - 1 < 0) {
+        paths.at(i,j) = paths.at(i-1,j) + temp;
+      } else {
+        paths.at(i,j) = MIN(paths.at(i, j-1) + temp, paths.at(i-1,j) + temp);
+      }
+    }
+  }
+  return MAX(paths.at(0,0), paths.at(M-1, N-1));
 }
 #endif
